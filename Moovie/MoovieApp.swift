bba8437ab756394be900169323ba21bc7982de36
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-03-26 12:47:52 +0700

    Initial Commit

---
+++ Moovie/MoovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoovieApp.swift
+//  Moovie
+//
+//  Created by Putut Yusri Bahtiar on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MoovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
