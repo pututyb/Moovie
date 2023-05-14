63971e6269f64080bb65833b23d6ea7b17dc49af
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-05-14 23:16:14 +0700

    .

---
+++ Moovie/Views/MyTicketView.swift
@@ -0,0 +1,20 @@
+//
+//  MyTicketView.swift
+//  Moovie
+//
+//  Created by Putut Yusri Bahtiar on 14/05/23.
+//
+
+import SwiftUI
+
+struct MyTicketView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MyTicketView_Previews: PreviewProvider {
+    static var previews: some View {
+        MyTicketView()
+    }
+}
---
+++ Moovie/Views/TicketDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  TicketDetailsView.swift
+//  Moovie
+//
+//  Created by Putut Yusri Bahtiar on 14/05/23.
+//
+
+import SwiftUI
+
+struct TicketDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TicketDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        TicketDetailsView()
+    }
+}
